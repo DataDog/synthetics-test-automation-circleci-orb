cea7ccceb58849b4feb602d3d3697afffed4ac7e
jakepruitt <jake.pruitt@datadoghq.com>
2022-03-23 10:38:10 +0100

    Use quotes

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -43,7 +43,7 @@ RunTests() {
 
     set +e
 
-    NODE_ENV=dev DEBUG=* \
+    NODE_ENV="dev" DEBUG="*" \
     DATADOG_API_KEY="${PARAM_API_KEY}" \
     DATADOG_APP_KEY="${PARAM_APP_KEY}" \
     DATADOG_SUBDOMAIN="${PARAM_SUBDOMAIN}" \
