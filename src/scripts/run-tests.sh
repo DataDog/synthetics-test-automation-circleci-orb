bc8790e41912014f398dfae3413d7da6c2e296a1
corentin <corentin.girard@datadoghq.com>
2023-05-22 16:32:03 +0200

    Actual bump that should have been done

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -6,7 +6,7 @@ RunTests() {
         PARAM_SITE=${DD_SITE}
     fi
 
-    DATADOG_CI_VERSION="2.10.0"
+    DATADOG_CI_VERSION="2.11.0"
     curl -L --fail "https://github.com/DataDog/datadog-ci/releases/download/v${DATADOG_CI_VERSION}/datadog-ci_linux-x64" --output "./datadog-ci"
 
     chmod +x ./datadog-ci
