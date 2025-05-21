2135cda0e40d8b8f594db775e76d62bfe164ac78
ci.datadog-ci <packages@datadoghq.com>
2025-05-21 16:08:16 +0000

    [dep] Bump datadog-ci to `3.6.1`

--- src/scripts/run-tests.sh
+++ src/scripts/run-tests.sh
@@ -6,7 +6,7 @@ RunTests() {
         PARAM_DATADOG_SITE=${DD_SITE}
     fi
 
-    DATADOG_CI_VERSION="3.5.0"
+    DATADOG_CI_VERSION="3.6.1"
 
     # Not run when running unit tests.
     if [[ -z "${DATADOG_CI_COMMAND}" ]]; then
